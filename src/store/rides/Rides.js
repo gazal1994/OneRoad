33abeff53becf2bed4db065e2866f67c8d36996f
gazal-eg <gazal.eg.94@gmail.com>
2020-06-30 23:33:02 +0300

    users-ride

--- src/store/rides/Rides.js
+++ src/store/rides/Rides.js
@@ -1,9 +1,32 @@
 import { observable, action, computed } from 'mobx'
 import {Ride} from './Ride'
 export class Rides {
-       
+       @observable rides=[]
 
-      constructor (){
-         
-      }
+    
+
+       @action getRides = async ()=>{
+            let json = await axios.get('http://localhost:3200/getRides');
+             json.data[0].forEach(m=>this.rides.push(new Ride(m.id,m.location,m.destination,m.departureTime,m.driver,m.status,m.distance)))        
+        }
+        @action addRide = async(id,location,destination,departureTime,driver,status,distance)=>{
+            let temp = {id,location,destination,departureTime,driver,status,distance}
+            await axios.post('http://localhost:3200/addRide', temp);
+          }
+        @action removeRide = async (id)=>{
+            let temp = {id:id}
+           await axios.DELETE('http://localhost:3200/Ride', temp);
+         }
+         @action approveRide = async (rideId,passengerId)=>{
+            let temp = {rideId,passengerId}
+           await axios.put('http://localhost:3200/approveRide', temp);
+         }
+         @action requestRide = async (rideId,passenger)=>{
+            let temp = {rideId,passenger}
+           await axios.put('http://localhost:3200/requestRide', temp);
+         }
+         @action finishRide = async (rideId)=>{
+            let temp = {rideId}
+           await axios.put('http://localhost:3200/requestRide', temp);
+         }
 }
--- src/store/users/Users.js
+++ src/store/users/Users.js
@@ -3,8 +3,13 @@ import {User} from './User'
 
 export class Users {
        @observable users=[]
-
-      constructor (){
-         
-      }
+      
+       @action addAllUsers = async ()=>{
+            let json = await axios.get('http://localhost:3200/Users');
+             json.data[0].forEach(m=>this.users.push(new User(m.id,m.name,m.phone,m.income,m.expense)))        
+        }
+        @action removeUser = async (id)=>{
+            let temp = {id:id}
+           await axios.DELETE('http://localhost:3200/removeUser', temp);
+         }
 }
