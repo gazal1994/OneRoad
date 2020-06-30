ba4f7c72b53275c2a133ff1ab35e240143528e61
davidy22118 <davidy22118@gmail.com>
2020-06-30 20:52:16 +0300

    start

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'mobx-react'
-//import { Item } from './store/Item'
-import { Inventory  } from './store/Inventory'
 
-let storeList = new Inventory()
+
+
 const stores = { storeList }
 ReactDOM.render(
     <Provider {...stores}>
