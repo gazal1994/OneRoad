a54765c4739a251c75b2c07ce53e0969aa625110
davidy22118 <davidy22118@gmail.com>
2020-07-01 12:29:10 +0300

    test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ const App = inject(
   "rides"
 )(
   observer((props) => {
+     props.users.getUsers()
+     console.log(props.users.users)
+     props.users.removeUser(3)
+     console.log(props.users.users)
+     props.rides.getRides()
+     console.log(props.rides.rides)
+     props.rides.removeRide(3)
+     props.rides.requestRide(2,props.users.users[0])
+     props.rides.addRide(9,"ashkelon","Tel Aviv","2020-07-09T21:00:00.000Z",'David B',false,45)
     return (
       <React.Fragment>
         <CssBaseline />
--- src/store/users/Users.js
+++ src/store/users/Users.js
@@ -1,20 +1,22 @@
 import { observable, action, computed } from 'mobx'
 import {User} from './User'
+import users from './users.json'
 const axios = require('axios')
 export class Users {
        @observable users=[]
       
-       @action addAllUsers = async ()=>{
-            const useraData = await axios.get('http://localhost:3200/Users');
+       @action getUsers = async ()=>{
+            const useraData = users /* await axios.get('http://localhost:3200/Users'); */
             const usersArry=[]
-            useraData.data[0].forEach(m=>usersArry.push(new User(m.id,m.name,m.phone,m.income,m.expense)))        
+            useraData/* .data[0] */.forEach(m=>usersArry.push(new User(m.id,m.name,m.phone,m.income,m.expense)))        
             this.users=usersArry
         }
         //addUser
-        @action removeUser = async (id)=>{
+      @action removeUser = async (id)=>{
            const delteUserId = {id:id}
-           const resiveUserInfo=await axios.DELETE('http://localhost:3200/removeUser', delteUserId);
-           const index=this.users.findIndex(u=>u.id=resiveUserInfo.data[0].id)
+          /*  const resiveUserInfo=await axios.DELETE('http://localhost:3200/removeUser', delteUserId); */
+           const index=this.users.findIndex(u=>u.id=== delteUserId.id/* resiveUserInfo.data[0].id */)
            this.users.splice(index,1)
          }
-}
+ 
+        }
