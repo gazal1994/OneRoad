ec6fbd6d71def7c512af39dd762c0fcdd8070dfc
Samer <samahajna@gmail.com>
2020-07-02 17:26:02 +0300

    connectbackfront

--- src/Components/Operations/Operation.jsx
+++ src/Components/Operations/Operation.jsx
@@ -1,37 +1,52 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { observer, inject } from "mobx-react";
 import { Link } from "react-router-dom";
 import Input from '../Common/Input'
 import CommonButton from "../Common/CommonButton";
 import TextField from "@material-ui/core/TextField";
 import Driver from '../../store/users/users.json'
+import Button from "@material-ui/core/Button";
+import AvailableRide from "./AvailableRide"
 
-const Operation= inject(
+const Operation = inject(
   "users",
   "rides"
 )(
   observer((props) => {
-      const driver=Driver[0]
-      const [textInput, setTextInput] = useState({location:'',destination:'',departureTime:''});
-      const handleChange = (e) => {
+    const driver = Driver[0]
+    const [relevantRides, setRelevantRides] = useState([])
+    const [textInput, setTextInput] = useState({ location: '', destination: '', departureTime: '' });
+    const handleChange = (e) => {
       const name = e.target.name;
-      setTextInput({...textInput,[name]:e.target.value});
-      props.rides.pushTohandelInput(textInput)
+      setTextInput({ ...textInput, [name]: e.target.value });
+    }
+    const handelClick = () => {
+      if (props.match.params.type == 'driver') {
+        props.rides.addRide(textInput)
       }
+      else {
+        console.log(textInput);
 
-      console.log(props.rides.handelInput)
-
-  const checkTypeUser=()=>{
-      if(props.match.params.type=='passenger'){
-          return 'Search'
-      }else{
-          return 'declare ride'
+        const relevant = props.rides.rides.filter(r =>
+          r.location == textInput.location &&
+          r.destination == textInput.destination //&&
+          // r.departureTime == textInput.departureTime
+        )
+        console.log(relevant);
+        setRelevantRides([...relevant])
       }
-  }
-      
+    }
+    const checkTypeUser = () => {
+      if (props.match.params.type == 'passenger') {
+        return 'Search'
+      } else {
+        return 'declare ride'
+      }
+    }
+
     return (
       <div>
-     { props.match.params.type}
+        {props.match.params.type}
           location:<TextField
           id="outlined-textarea"
           label="Multiline Placeholder"
@@ -39,7 +54,7 @@ const Operation= inject(
           multiline
           variant="outlined"
           value={textInput.location}
-          name= "location"
+          name="location"
           onChange={handleChange}
         />
           destination:<TextField
@@ -49,7 +64,7 @@ const Operation= inject(
           multiline
           variant="outlined"
           value={textInput.destination}
-          name= "destination"
+          name="destination"
           onChange={handleChange}
         />
           departure time:<TextField
@@ -59,10 +74,12 @@ const Operation= inject(
           multiline
           variant="outlined"
           value={textInput.departureTime}
-          name= "departureTime"
+          name="departureTime"
           onChange={handleChange}
         />
-     {/* <Link to='/Landing'> */} <CommonButton textInput={textInput} setTextInput={setTextInput} operation={props.match.params.type} name = {checkTypeUser()}/>{/* </Link> */}
+        <Button onClick={handelClick} variant="contained" color="primary">Search</Button>
+        {relevantRides.map(r => <AvailableRide key={r.id} ride={r} />)}
+
       </div>
     );
   })
