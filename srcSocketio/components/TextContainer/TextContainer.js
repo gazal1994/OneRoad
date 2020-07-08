51a28c362fab803fecc9c30a0a5e564e77ebc464
gazal-eg <gazal.eg.94@gmail.com>
2020-07-08 21:23:03 +0300

    socket

---
+++ srcSocketio/components/TextContainer/TextContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+ 
+    <div>
+    
+    </div>
+
+  
+);
+
+export default TextContainer;
